32879a3b322b13e324f06cfba528c424be03e480
Aluno <Aluno@LAB04-PC08>
2024-06-14 19:43:15 -0300

    Commit Input03

---
+++ src/Input03.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+class Input03 {
+
+    public static void main(String[] args) {
+        //Criando um scanner
+        Scanner sc = new Scanner (System.in);
+        System.out.println("digite primeiro numero:");
+        int n1 = sc.nextInt();
+        System.out.println("digite primeiro numero:");
+        int n2 = sc.nextInt();
+        System.out.println("digite primeiro numero:");
+        int n3 = sc.nextInt();
+        System.out.println("a soma dos numeros e igual a :" + (n1+n2+n3));
+        sc.close();
+        
+        
+        
+        //Encontre e imprima a soma de três números inteiros inseridos pelo usuário
+        
+        
+        
+        
+        //Remember to close the Scanner
+        
+    }
+}
